2835374f51196248a0ad69791d8e64e334a3d57d
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2023-11-13 22:55:01 +0530

    added package and folders for projects

---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String imageurl =
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGkAznCVTAALTD1o2mAnGLudN9r-bY6klRFB35J2hY7gvR9vDO3bPY_6gaOrfV0IHEIUo&usqp=CAU';
+
+  static String apiKey = 'd202b079f10a409e9b276c3562be61be';
+}
